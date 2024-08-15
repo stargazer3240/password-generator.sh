f429941e28e3cb3ffe578e774284c6220fa1b457
Stargazer3240 <112516550+Stargazer3240@users.noreply.github.com>
2024-08-15 10:57:58 -0300

    Parsing e help implementados

--- password-generator.sh
+++ password-generator.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-# Função para exibir a ajuda
 show_help() {
-  # Implementação vai aqui
+  echo "Bem vindo ao password-generator! Versão 1.0, (c) 2024, Fabrício Moura Jácome, DIMAp, UFRN
+Uso: ./password-generator.sh [OPÇÕES]
+Opções:
+  -l [COMPRIMENTO] : comprimento da senha
+  -u               : incluir letras maiúsculas
+  -d               : incluir números
+  -s               : incluir símbolos
+  -h               : exibir essa mensagem de ajuda
+
+O comportamento padrão do script é gerar uma senha de 8 caracteres minúsculos."
 }
 
 # Definir variáveis padrão
@@ -11,7 +19,25 @@ USE_DIGITS=false
 USE_SYMBOLS=false
 
 # Parsear argumentos
-# { Implementação vai aqui }
+while getopts ":l:udsh" opt; do
+  case "$opt" in
+  l)
+    LENGTH=$OPTARG
+    ;;
+  u)
+    USE_UPPERCASE=true
+    ;;
+  d)
+    USE_DIGITS=true
+    ;;
+  s)
+    USE_SYMBOLS=true
+    ;;
+  h)
+    show_help
+    ;;
+  esac
+done
 
 # Definir conjuntos de caracteres
 LOWERCASE="abcdefghijklmnopqrstuvwxyz"
